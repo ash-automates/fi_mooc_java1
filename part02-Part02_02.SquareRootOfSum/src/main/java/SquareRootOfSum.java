5501f23208e243fe88a789fcc029291058fde01e
Hachem S <s.hachem@murena.io>
2024-09-25 17:09:47 +0300

    Submit solution for Ex 02-02

--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -6,5 +6,8 @@ public class SquareRootOfSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int first = Integer.valueOf(scanner.nextLine());
+        int second = Integer.valueOf(scanner.nextLine());
+        System.out.println(Math.sqrt(first + second));
     }
 }
