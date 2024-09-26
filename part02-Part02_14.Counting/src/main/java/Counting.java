ddf84c9a8563ecafe1eb26fc6925d102cae9b284
Hachem S <s.hachem@murena.io>
2024-09-26 16:36:53 +0300

    Submit solution for Ex 02-14

--- part02-Part02_14.Counting/src/main/java/Counting.java
+++ part02-Part02_14.Counting/src/main/java/Counting.java
@@ -5,6 +5,11 @@ public class Counting {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int end = Integer.valueOf(scanner.nextLine());
+        int iteration = 0;
+        while (iteration <= end) {
+            System.out.println(iteration);
+            iteration++;
+        }
     }
 }
