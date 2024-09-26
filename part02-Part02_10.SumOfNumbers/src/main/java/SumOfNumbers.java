bdff8cad393e5a3b53ccd78f4545339aebd0a5f5
Hachem S <s.hachem@murena.io>
2024-09-26 09:03:37 +0300

    Submit solution for Ex 02-10

--- part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -5,6 +5,28 @@ public class SumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        
+        
+        while (true) {
+            System.out.println("Give a number:");
+            // The task is to read a user inputted number
+            int number = Integer.valueOf(scanner.nextLine());
+
+            // The task is to exit the loop if the user
+            // has inputted zero
+            if (number == 0) {
+                break;
+            }
+            
+            // The task is to sum up the values
+            // if the user inputs a number
+            sum = sum + number;
+            
+        }
+
+        // The task is to print out the sum
+        System.out.println("Sum of the numbers: " + sum);
 
     }
 }
