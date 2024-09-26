a9b95a11ba14d9720dac39e1be05ae847c48ca51
Hachem S <s.hachem@murena.io>
2024-09-26 11:02:41 +0300

    Submit solution for Ex 02-12

--- part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,21 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int values = 0;
+        int sum = 0;    
+        
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == 0) {
+                break;
+            }
+             
+            values = values + 1;
+            sum = sum + number;
+            
+        }
 
+        System.out.println("Average of the numbers: " + ((double) sum / values));
     }
 }
