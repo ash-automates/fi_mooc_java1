e19f6b059109c1a175b41f174bebf7dc4b4724af
Hachem S <s.hachem@murena.io>
2024-09-26 08:43:44 +0300

    Submit solution for Ex 02-08

--- part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,27 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int values = 0;
+        
+        
+        while (true) {
+            System.out.println("Give a number:");
+            // The task is to read a user inputted number
+            int number = Integer.valueOf(scanner.nextLine());
 
+            // The task is to exit the loop if the user
+            // has inputted zero
+            if (number == 0) {
+                break;
+            }
+
+            // The task is to increase the amount of ones
+            // if the user inputs a number one
+            values = values + 1;
+            
+        }
+
+        // The task is to print out the total of ones
+        System.out.println("Number of numbers: " + values);
     }
 }
