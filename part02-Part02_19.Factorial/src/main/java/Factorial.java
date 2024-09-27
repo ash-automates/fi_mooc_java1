349e3864846479d0462439a089820938e94d5b62
Hachem S <s.hachem@murena.io>
2024-09-27 11:16:43 +0300

    Submit solution for Ex 02-20

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -5,6 +5,20 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.print("Give a number: ");
+        int n = Integer.valueOf(scanner.nextLine());
+        int product;
+        if (n == 0) {
+            product = 0;
+        } else {
+            product = 1;
+            int step = 1;
+            while (step <= n) {
+                product = product * step;
+                step = step + 1;
+            }
+        }
+        
+        System.out.println("Factorial: " + product);
     }
 }
