515bd5f832ab3ec9ce5fd674b7da2a06be1b0449
Hachem S <s.hachem@murena.io>
2024-09-27 10:49:09 +0300

    Submit solution for Ex 02-18

--- part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -5,6 +5,15 @@ public class SumOfASequenceTheSequel {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.print("First number? ");
+        int start = Integer.valueOf(scanner.nextLine());
+        System.out.print("Last number? ");
+        int end = Integer.valueOf(scanner.nextLine());
+        int sum = 0;
+        while (start <= end) {
+            sum = sum + start;
+            start = start + 1;
+        }
+        System.out.println("The sum is " + sum);
     }
 }
