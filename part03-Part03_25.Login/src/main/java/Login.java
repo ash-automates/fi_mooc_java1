d4a2de3e87ddb53c2c6a9f7d7b149cf0f782854b
Hachem S <s.hachem@murena.io>
2024-10-06 01:19:44 +0300

    Submit solution for Ex 03-25

--- part03-Part03_25.Login/src/main/java/Login.java
+++ part03-Part03_25.Login/src/main/java/Login.java
@@ -5,6 +5,14 @@ public class Login {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.print("Enter username: ");
+        String usr = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String pwd = scanner.nextLine();
+        if ((usr.equals("alex") && pwd.equals("sunshine")) || (usr.equals("emma") && pwd.equals("haskell"))) {
+            System.out.println("You have successfully logged in!");
+        } else {
+            System.out.println("Incorrect username or password!");
+        }
     }
 }
