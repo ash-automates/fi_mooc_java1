f5dbd98008fcfdbfbbed89c2490a3eff25a521ae
Hachem S <s.hachem@murena.io>
2024-10-06 20:24:56 +0300

    Submit solution for Ex 04-02

--- part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -4,5 +4,11 @@ public class YourFirstBankTransfer {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // write your program here
+        Account mat = new Account("Matthews account", 1000.00);
+        Account me = new Account("My account", 0.00);
+        mat.withdrawal(100);
+        me.deposit(100);
+        System.out.println(mat);
+        System.out.println(me);
     }
 }
