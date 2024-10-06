5e636bef8aff94197ef0aae857bd4afe40d6c987
Hachem S <s.hachem@murena.io>
2024-10-06 06:42:21 +0300

    Submit solution for Ex 03-32

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -6,7 +6,25 @@ public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        String longestName = "";
+        int longestNameSize = 0;
+        int sumOfYears = 0;
+        int count = 0;
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            String[] fragments = input.split(",");
+            sumOfYears += Integer.valueOf(fragments[1]);
+            count += 1;
+            if (fragments[0].length() > longestNameSize) {
+                longestName = fragments[0];
+                longestNameSize = fragments[0].length();
+            }
+        }
+        System.out.println("Longest name: " + longestName);
+        System.out.println("Average of the birth years: " + (1.0 * sumOfYears / count));
     }
 }
