95ee299153c1b8227fc5b1457da494a75dd45022
Hachem S <s.hachem@murena.io>
2024-10-06 20:15:51 +0300

    Submit solution for Ex 04-01

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,8 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        Account myAccount = new Account("My account", 100.00);
+        myAccount.deposit(20);
+        System.out.println(myAccount);
     }
 }
