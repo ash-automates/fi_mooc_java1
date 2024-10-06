712059c84f6a51ddbf5793428055cb4933b58b55
Hachem S <s.hachem@murena.io>
2024-10-06 06:20:40 +0300

    Submit solution for Ex 03-30

--- part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -6,6 +6,14 @@ public class LastWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
 
+            String[] fragments = input.split(" ");
+            System.out.println(fragments[fragments.length - 1]);
+        }
     }
 }
--- part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -5,7 +5,18 @@ public class AgeOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int ageOfOldest = 0;
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            String[] fragments = input.split(",");
+            if (Integer.valueOf(fragments[1]) > ageOfOldest) {
+                ageOfOldest = Integer.valueOf(fragments[1]);
+            }
+        }
+        System.out.println("Age of the oldest: " + ageOfOldest);
 
     }
 }
