0fc546c47e071a8b8f268d2daf172fa3153712e2
Hachem S <s.hachem@murena.io>
2024-10-06 06:30:05 +0300

    Submit solution for Ex 03-31

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -5,7 +5,20 @@ public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int ageOfOldest = 0;
+        String nameOfOldest = "";
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            String[] fragments = input.split(",");
+            if (Integer.valueOf(fragments[1]) > ageOfOldest) {
+                nameOfOldest = fragments[0];
+                ageOfOldest = Integer.valueOf(fragments[1]);
+            }
+        }
+        System.out.println("Name of the oldest: " + nameOfOldest);
 
     }
 }
