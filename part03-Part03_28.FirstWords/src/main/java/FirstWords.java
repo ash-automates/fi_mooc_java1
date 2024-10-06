165af36751778755a317a1d715a7257c127a3754
Hachem S <s.hachem@murena.io>
2024-10-06 05:52:03 +0300

    Submit solution for Ex 03-28

--- part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -6,6 +6,19 @@ public class FirstWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
 
+        if (input.equals("")) {
+            return;
+        }
+        
+        String[] fragments = input.split(" ");
+        int index = 0;
+        while (!(input.equals("")) && index < fragments.length) {
+            System.out.println(fragments[0]);
+            input = scanner.nextLine();
+            fragments = input.split(" ");
+            index = 0;
+        }
     }
 }
