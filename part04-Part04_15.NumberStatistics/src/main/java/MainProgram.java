e96322187250e9b4e6ebd193f502fdb87fa9e0da
Hachem S <s.hachem@murena.io>
2024-10-10 07:50:12 +0300

    Submit solution for Ex 04-15

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -12,5 +12,24 @@ public class MainProgram {
         // correct order in the main program. First the object that tracks the total
         // sum, secondly the object that tracks the sum of even numbers, 
         // and lastly the one that tracks the sum of odd numbers!
+        Statistics stats = new Statistics();
+        Statistics evenStats = new Statistics();
+        Statistics oddStats = new Statistics();
+        System.out.println("Enter numbers:");
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+            stats.addNumber(input);
+            if (input % 2 == 0) {
+                evenStats.addNumber(input);
+            } else {
+                oddStats.addNumber(input);
+            }
+        }
+        System.out.println("Sum: " + stats.sum());
+        System.out.println("Sum of even numbers: " + evenStats.sum());
+        System.out.println("Sum of odd numbers: " + oddStats.sum());
     }
 }
