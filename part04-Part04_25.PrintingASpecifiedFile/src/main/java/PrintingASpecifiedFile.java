6635b6499ab1e71481c3703e2295f470d8864735
Hachem S <s.hachem@murena.io>
2024-10-11 17:31:21 +0300

    Submit solution for Ex 04-25

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -5,7 +5,14 @@ import java.util.Scanner;
 public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner scannerUsr = new Scanner(System.in);
+        String input = scannerUsr.nextLine();
+        try (Scanner scanner = new Scanner(Paths.get(input))) {
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
