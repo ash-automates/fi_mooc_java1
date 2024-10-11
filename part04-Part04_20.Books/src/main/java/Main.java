e69b6ac7c2dfa01b15c4cdbf9ee03590113fc2e8
Hachem S <s.hachem@murena.io>
2024-10-11 12:57:50 +0300

    Submit solution for Ex 04-20

--- part04-Part04_20.Books/src/main/java/Main.java
+++ part04-Part04_20.Books/src/main/java/Main.java
@@ -7,6 +7,29 @@ public class Main {
         
         // implement here the program that allows the user to enter 
         // book information and to examine them
-
+        ArrayList<Book> books = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("Title:");
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+            System.out.println("Pages:");
+            int pages = Integer.valueOf(scanner.nextLine());
+            System.out.println("Publication year:");
+            int year = Integer.valueOf(scanner.nextLine());
+            books.add(new Book(name, pages, year));
+        }
+        System.out.println("What information will be printed?");
+        String toPrint = scanner.nextLine();
+        for (Book book: books) {
+            if (toPrint.equals("everything")) {
+                System.out.println(book);
+            } else if (toPrint.equals("name")) {
+                System.out.println(book.getName());
+            }
+        }
     }
 }
