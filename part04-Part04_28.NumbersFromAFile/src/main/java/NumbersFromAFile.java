0a7a71ccdcd48604e918d97b958af9431ada1056
Hachem S <s.hachem@murena.io>
2024-10-12 03:33:32 +0300

    Submit solution for Ex 04-28

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -9,11 +9,21 @@ public class NumbersFromAFile {
 
         System.out.print("File? ");
         String file = scanner.nextLine();
-        System.out.print("Lower bound? ");
-        int lowerBound = Integer.valueOf(scanner.nextLine());
-        System.out.print("Upper bound? ");
-        int upperBound = Integer.valueOf(scanner.nextLine());
-
+        try (Scanner fileScanner = new Scanner(Paths.get(file))) {
+            System.out.print("Lower bound? ");
+            int lowerBound = Integer.valueOf(scanner.nextLine());
+            System.out.print("Upper bound? ");
+            int upperBound = Integer.valueOf(scanner.nextLine());
+            int count = 0;
+            while (fileScanner.hasNextLine()) {
+                int num = Integer.valueOf(fileScanner.nextLine());
+                if (num >= lowerBound && num <= upperBound) {
+                    count = count + 1;
+                }
+            }
+            System.out.println("Numbers: " + count);
+        } catch (Exception e) {
+            System.out.println("Reading the file " + file + " failed.");
+        }
     }
-
 }
