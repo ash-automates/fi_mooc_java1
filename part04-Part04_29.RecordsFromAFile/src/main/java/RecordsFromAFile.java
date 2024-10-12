eed9381674e17aaf81dc31b228acac3c721037b8
Hachem S <s.hachem@murena.io>
2024-10-12 03:55:13 +0300

    Submit solution for Ex 04-29

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -5,7 +5,19 @@ import java.util.Scanner;
 public class RecordsFromAFile {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.println("Name of the file:");
+        String input = inputScanner.nextLine();
+        try (Scanner scanner = new Scanner(Paths.get(input))) {
+            while (scanner.hasNextLine()) {
+                String[] parts = scanner.nextLine().split(",");
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
+                String pluralize = age == 1 ? "year" : "years";
+                System.out.println(name + ", age: " + age + " " + pluralize);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
