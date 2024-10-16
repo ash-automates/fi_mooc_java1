7563d99199d698ce1bbefd85a562794e28633d54
Hachem S <s.hachem@murena.io>
2024-10-16 04:38:53 +0300

    Submit solution for Ex 05-07

--- part05-Part05_07.NullPointerException/src/main/java/NullPointerExceptionProgram.java
+++ part05-Part05_07.NullPointerException/src/main/java/NullPointerExceptionProgram.java
@@ -6,7 +6,8 @@ public class NullPointerExceptionProgram {
     public static void main(String[] args) {
         // Implement a program, or modify a program so, that it causes 
         // the NullPointerException -error
-
+        String erronous = null;
+        System.out.println(erronous.isEmpty());
         ArrayList<String> lines = new ArrayList<>();
         lines.add("Never has a man influenced physics so profoundly as Niels Bohr in the early 1900's");
         lines.add("Going back to this time period, little was known about atomic structure; Bohr set out");
