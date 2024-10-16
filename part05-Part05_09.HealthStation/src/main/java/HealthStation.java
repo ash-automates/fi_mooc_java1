6c68b01d3298d0621a16af9f295c588bdbb9ed31
Hachem S <s.hachem@murena.io>
2024-10-16 05:15:57 +0300

    Submit solution for Ex 05-09

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,19 @@
 
 public class HealthStation {
+    private int weighins;
 
 
     public int weigh(Person person) {
         // return the weight of the person passed as the parameter
-        return -1;
+        this.weighins += 1;
+        return person.getWeight();
     }
-
+    
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+    
+    public int weighings() {
+        return this.weighins;
+    }
 }
