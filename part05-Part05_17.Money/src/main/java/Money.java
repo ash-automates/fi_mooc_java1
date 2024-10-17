998baebb6d1a835a1461d8140fc0b9ca7301cbeb
Hachem S <s.hachem@murena.io>
2024-10-17 22:36:02 +0300

    Submit solution for Ex 05-17

--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -32,4 +32,38 @@ public class Money {
         return this.euros + "." + zero + this.cents + "e";
     }
 
+    public Money plus(Money addition) {
+        int eurSum = this.euros + addition.euros;
+        int centSum = this.cents + addition.cents;
+        if (centSum > 99) {
+            centSum = centSum % 100;
+            eurSum = eurSum + 1;
+        }
+        Money newMoney = new Money(eurSum, centSum); // create a new Money object that has the correct worth
+
+        // return the new Money object
+        return newMoney;
+    }
+    
+    public boolean lessThan(Money compared) {
+        if (this.euros == compared.euros) {
+            return this.cents < compared.cents;
+        } else {
+            return this.euros < compared.euros;
+        }
+    }
+    
+    public Money minus(Money decreaser) {
+        if (this.lessThan(decreaser)) {
+            return new Money(0, 0);
+        }
+        int eurDiff = this.euros - decreaser.euros;
+        int centDiff = this.cents - decreaser.cents;
+        if (centDiff < 0) {
+            eurDiff = eurDiff - 1;
+            centDiff = 100 + centDiff;
+        }
+        Money newMoney = new Money(eurDiff, centDiff);
+        return newMoney;
+    }
 }
