7d61204b382237d054f23e8afec6b3965fcac9d5
Hachem S <s.hachem@murena.io>
2024-10-18 17:48:39 +0300

    Submit solution for Ex 06-05

---
+++ part06-Part06_05.SantasWorkshop/src/main/java/Package.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.ArrayList;
+/**
+ *
+ * @author hachem
+ */
+public class Package {
+    private ArrayList<Gift> gifts;
+
+    public Package() {
+        this.gifts = new ArrayList<>();
+    }
+    
+    public void addGift(Gift gift) {
+        gifts.add(gift);
+    }
+    
+    public int totalWeight() {
+        int sum = 0;
+        for (Gift gift: gifts) {
+            sum += gift.getWeight();
+        }
+        return sum;
+    }
+}
