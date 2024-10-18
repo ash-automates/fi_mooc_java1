e4fdab0ab62773e676d88205ba2ef85a01af9f85
Hachem S <s.hachem@murena.io>
2024-10-18 11:05:19 +0300

    Submit solution for Ex 06-02

---
+++ part06-Part06_02.Stack/src/main/java/Stack.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.ArrayList;
+/**
+ *
+ * @author hachem
+ */
+public class Stack {
+    private ArrayList<String> stack;
+    
+    public Stack() {
+        this.stack = new ArrayList<>();
+    }
+    
+    public boolean isEmpty() {
+        return this.stack.isEmpty();
+    }
+    
+    public void add(String value) {
+        this.stack.add(value);
+    }
+    
+    public ArrayList<String> values() {
+        return this.stack;
+    }
+    
+    public String take() {
+        return this.stack.remove(this.stack.size() - 1);
+    }
+}
