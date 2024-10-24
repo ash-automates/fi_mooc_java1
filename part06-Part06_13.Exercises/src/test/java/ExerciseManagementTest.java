c91a10d982b699750e70847a08cd671184e57d2f
Hachem S <s.hachem@murena.io>
2024-10-25 02:08:47 +0300

    Submit solution for Ex 06-13

--- part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
+++ part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
@@ -1,6 +1,46 @@
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import org.junit.Test;
+import org.junit.Before;
 
 
 public class ExerciseManagementTest {
-
+    private ExerciseManagement em;
+    
+    @Before
+    public void initialize() {
+        em = new ExerciseManagement();
+    }
+    
+    @Test
+    public void exerciseListEmptyAtBeginning() {
+        assertEquals(0, em.exerciseList().size());
+    }
+    
+    @Test
+    public void addingExerciseGrowsListByOne() {
+        em.add("Write a test");
+        assertEquals(1, em.exerciseList().size());
+    }
+    
+    @Test
+    public void addedExerciseIsInList() {
+        em.add("Write a test");
+        assertTrue(em.exerciseList().contains("Write a test"));
+    }
+    
+    @Test
+    public void exerciseCanBeMarkedAsCompleted() {
+        em.add("New Exercise");
+        em.markAsCompleted("New Exercise");
+        assertTrue(em.isCompleted("New Exercise"));
+    }
+    
+    @Test
+    public void ifNotMarkedCompletedIsNotCompleted() {
+        em.add("New Exercise");
+        em.markAsCompleted("New Exercise");
+        assertFalse(em.isCompleted("Another Exercise"));
+    }
 }
